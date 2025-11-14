e47df322ab37fcaa8219276d4d3de479b681e03d
Saminathan-Palanisamy <saminathan@innoboon.com>
2025-11-14 13:49:11 +0530

    Streamlit- Register, login and logout are successful.

--- streamlit_app/streamlit.py
+++ streamlit_app/streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 from PIL import Image 
-
+from fastapi import HTTPException
 
 BASE_URL = "http://127.0.0.1:8000"
 
@@ -27,6 +27,25 @@ def go_login():
 
 def go_register():
     st.session_state.page = "register"
+
+#logout function
+def Logout_user():
+   
+    try:
+        headers = {"Authorization": f"Bearer {st.session_state.token}"}
+        res = requests.post(f"{BASE_URL}/users/logout", headers=headers)
+        if res.status_code == 200:
+                
+            st.toast("Logout successful!")
+            st.session_state.is_logged_in = False
+            st.session_state.token = None
+            st.session_state.page = "home"
+            st.rerun()               
+        else:
+            st.error(res.json().get("detail"))
+
+    except Exception as e:
+        st.error(f"Logout Error: {e}")
 #-----------------home set up----------------------
 if st.session_state.page == "home" and not st.session_state.is_logged_in:
     st.title(" INMAR Authentication Portal")
@@ -46,20 +65,25 @@ elif st.session_state.page == "login" and not st.session_state.is_logged_in:
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
 
+
     if st.button("Login"):
         payload = {"email": email, "password": password}
 
         try:
             res = requests.post(f"{BASE_URL}/users/login", data=payload)
             
+            
             if res.status_code == 200:
                 data = res.json()
+                
                 st.success(data["message"])
 
                 st.session_state.token = data["access_token"]
                 st.session_state.user_email = data["email"]
                 st.session_state.is_logged_in = True
                 st.session_state.page = "dashboard"
+                st.rerun()
+                
 
             else:
                 st.error(res.json().get("detail"))
@@ -91,7 +115,9 @@ elif st.session_state.page == "register" and not st.session_state.is_logged_in:
         try:
             res = requests.post(f"{BASE_URL}/users/register", json=payload)
             if res.status_code == 201:
-                st.success("Registration successful! You can now login.")
+                st.toast("Registration successful! You can now login.")
+                st.session_state.page = "login"
+                st.rerun()
             else:
                 st.error(res.json().get("detail"))
 
@@ -104,22 +130,17 @@ elif st.session_state.page == "register" and not st.session_state.is_logged_in:
 elif st.session_state.is_logged_in:
     st.title(" Welcome to INMAR Dashboard")
     st.success(f"Logged in as: {st.session_state.user_email}")
-    img = Image.open("dodge.jpg") 
-    st.image(img, width=1000, onclick=None)
+    try:
+        img = Image.open("dodge.jpg")
+        st.image(img, width=1000)
 
-    if st.button("Logout"):
-        try:
-            headers = {"Authorization": f"Bearer {st.session_state.token}"}
-            res = requests.post(f"{BASE_URL}/users/logout", headers=headers)
 
-            if res.status_code == 200:
-                st.success("Logout successful!")
-                st.session_state.is_logged_in = False
-                st.session_state.token = None
-                st.session_state.page = "home"
-            else:
-                st.error(res.json().get("detail"))
 
-        except Exception as e:
-            st.error(f"Error: {e}")
-# ------------------------------------------------
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"login error occured:{str(e)}")
+
+
+    if st.button("Logout"):    
+        Logout_user()
+
+#------------------------------------------
